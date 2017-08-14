c6a8e0112e20b9ba7aa56187b615f2e20686b961
Kris Chery <kris.chery2@gmail.com>
2017-08-14 16:29:40 -0500

    Fixing up heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :static_pages
-  root 'static_pages#home'
 
+  root 'static_pages#home'
+  get '/static_pages/index'
+  get '/static_pages/contact'
+  get '/static_pages/portfolio'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
